dd8a60225981e196464683a0a300ae25f6dd60ec
SaurabhS120 <saurabh.sonar120@gmail.com>
2024-07-13 17:02:03 +0530

    Notification cancel action

--- app/src/main/java/com/example/androidbackgroundprocessing/DummyDownloadService.kt
+++ app/src/main/java/com/example/androidbackgroundprocessing/DummyDownloadService.kt
@@ -1,8 +1,10 @@
 package com.example.androidbackgroundprocessing
 
 import android.Manifest
+import android.app.Notification
 import android.app.PendingIntent
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.content.pm.ServiceInfo
@@ -12,6 +14,7 @@ import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import com.example.androidbackgroundprocessing.DummyDownloadService.DownloadActions
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -88,12 +91,36 @@ class DummyDownloadService : Service() {
             }
         }
     }
-    val notificationBuilder = NotificationCompat
-        .Builder(this, "download_channel")
-        .setSmallIcon(R.drawable.ic_launcher_foreground)
-        .setContentTitle("Dummy Download")
-    fun buildNotification(progress: Int = 0) =
-        notificationBuilder
-        .setProgress(100, progress, progress==-1)
-        .build()
+
+    val notificationHelper = DownloadNotificationHelper()
+    fun buildNotification(progress: Int = 0)=notificationHelper.buildNotification(this,progress)
+}
+class DownloadNotificationHelper{
+    private var notificationBuilder : NotificationCompat.Builder? = null
+    fun buildNotification(context:Context,progress: Int = 0):Notification {
+        if(notificationBuilder==null){
+
+            val notificationCancelIntent: Intent =
+                Intent(context, DummyDownloadService::class.java).apply {
+                    action = DownloadActions.STOP.toString()
+                }
+            val notificationCancelPendingIntent: PendingIntent = PendingIntent.getService(
+                context,
+                0,
+                notificationCancelIntent,
+                PendingIntent.FLAG_IMMUTABLE
+            )
+            val notificationCancelAction =
+                NotificationCompat.Action(null, "Cancel", notificationCancelPendingIntent)
+
+            notificationBuilder = NotificationCompat
+                .Builder(context, "download_channel")
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("Dummy Download")
+                .addAction(notificationCancelAction)
+        }
+        return  notificationBuilder!!
+            .setProgress(100, progress, progress == -1)
+            .build()
+    }
 }
