6ab4af39f815a66b835e3c9812877f2786d60b43
SaurabhS120 <saurabh.sonar120@gmail.com>
2024-07-15 11:42:14 +0530

    DownloadProgressNotifierClientTest for onCreate and onUpdate

--- app/src/test/java/com/example/androidbackgroundprocessing/downloadProgressWatchers/DownloadProgressNotifierClientTest.kt
+++ app/src/test/java/com/example/androidbackgroundprocessing/downloadProgressWatchers/DownloadProgressNotifierClientTest.kt
@@ -8,6 +8,7 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
+import org.mockito.Mockito
 import org.mockito.Mockito.mock
 import org.mockito.junit.MockitoJUnitRunner
 
@@ -41,5 +42,20 @@ class DownloadProgressNotifierClientTest {
     @Test(expected = DownloadProgressWatcherNoContext::class)
     fun buildNotificationFailTest(){
         val notification = client.buildNotification()
+    }
+
+    @Test
+    fun onCreate(){
+        client.onCreate(mockContext)
+        Mockito.verify(downloadProgressBroadcastHelper,Mockito.times(1)).onCreate(mockContext)
+        Mockito.verify(downloadNotificationHelper,Mockito.times(1)).onCreate(mockContext)
+    }
+
+    @Test
+    fun onUpdate(){
+        client.onCreate(mockContext)
+        client.onUpdate(1)
+        Mockito.verify(downloadProgressBroadcastHelper,Mockito.times(1)).onUpdate(1)
+        Mockito.verify(downloadNotificationHelper,Mockito.times(1)).onUpdate(1)
     }
 }
